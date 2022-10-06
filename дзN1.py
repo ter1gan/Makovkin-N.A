a3548a6f0dda153f067223359a4fb0cbd7da8b8f
ter1gan <113279557+ter1gan@users.noreply.github.com>
2022-10-06 18:29:21 +0300

    Update дзN1.py

--- дзN1.py
+++ дзN1.py
@@ -1,31 +1,31 @@
 "# -- coding: utf-8 --"
-print('Курс Основы программирования начался')
-print(16823 * 12302 / 3092)
-age = int(input('Укажите свой возраст: '))
+print('Курс Основы программирования начался')                                              #1#
+print(16823 * 12302 / 3092)                                                                #2#
+age = int(input('Укажите свой возраст: '))                                                 #3#
 if age >= 16 and age < 75:
     print('Поздравляем вы поступили в ВГУИТ')
 elif age > 0 and age < 16:
     print('Сначала нужно окончить школу!')
 else:
     print('Введите возраст коректно')
-name = str(input('Укажите свое имя: '))
+name = str(input('Укажите свое имя: '))                                                   #4#
 if name == ('Иван'):
     print('Ваше имя не подходит')
 if age < 16:
     print('Вам осталось учиться в школе ',16-age,'лет')
-sekonds = int(input("Введите кол-во секунд "))
+sekonds = int(input("Введите кол-во секунд "))                                            #5#
 min = sekonds//60
 hours = min//60
 days = hours//24
 print(days, ":" , hours, ":", min, ":", sekonds )
-n = int(input('''Укажите число для решения выражения n + n^2 + n^3 + n^4 + n^5: '''))
+n = int(input('''Укажите число для решения выражения n + n^2 + n^3 + n^4 + n^5: '''))     #6#
 print(n + n**2 + n**3 + n**4 + n**5)
 a = 3
 b = 4
 a, b = b, a
 print(a)
 print(b)
-number = int(input('Введите число, '))
+number = int(input('Введите число, '))                                                   #7#
 if number % 2 == 0:
     print('Четное число')
 else:
