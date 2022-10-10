bd28ca59923b4b387060a777c79763535569eef2
ter1gan <113279557+ter1gan@users.noreply.github.com>
2022-10-10 19:23:43 +0300

    Update дзN1.py

--- дзN1.py
+++ дзN1.py
@@ -1,6 +1,7 @@
 "# -- coding: utf-8 --"
 print('Курс Основы программирования начался')                                              #1#
-print(16823 * 12302 / 3092)                                                                #2#
+a = 16823 * 12302 / 3092                                                                   #2#
+print(a)                                                                
 age = int(input('Укажите свой возраст: '))                                                 #3#
 if age >= 16 and age < 75:
     print('Поздравляем вы поступили в ВГУИТ')
@@ -8,24 +9,24 @@ elif age > 0 and age < 16:
     print('Сначала нужно окончить школу!')
 else:
     print('Введите возраст коректно')
-name = str(input('Укажите свое имя: '))                                                   #4#
+name = str(input('Укажите свое имя: '))                                                   
 if name == ('Иван'):
     print('Ваше имя не подходит')
 if age < 16:
     print('Вам осталось учиться в школе ',16-age,'лет')
-sekonds = int(input("Введите кол-во секунд "))                                            #5#
+sekonds = int(input("Введите кол-во секунд "))                                            #4#
 min = sekonds//60
 hours = min//60
 days = hours//24
 print(days, ":" , hours, ":", min, ":", sekonds )
-n = int(input('''Укажите число для решения выражения n + n^2 + n^3 + n^4 + n^5: '''))     #6#
+n = int(input('''Укажите число для решения выражения n + n^2 + n^3 + n^4 + n^5: '''))     #5#
 print(n + n**2 + n**3 + n**4 + n**5)
-a = 3
-b = 4
+a = 2                                                                                     #6#
+b = 1
 a, b = b, a
 print(a)
 print(b)
-number = int(input('Введите число, '))                                                   #7#
+number = int(input())                                                                      #7#
 if number % 2 == 0:
     print('Четное число')
 else:
