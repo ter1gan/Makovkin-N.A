3648d08c2451d70ba676363d170760361551125e
ter1gan <113279557+ter1gan@users.noreply.github.com>
2022-11-19 17:11:52 +0300

    Delete ПР 3 directory

--- ПР 3/задание 4/задание 4.py
+++
@@ -1,3 +0,0 @@
-def F(a, b, l, n):
-    return a + 2*n*a + 2*b*n + l
-print(F(2, 7, 4, 5))
--- ПР 3/задание 7/задание 7.py
+++
@@ -1,6 +0,0 @@
-def F(n):
-    if n % 4 == 0 and n % 100 != 0 or n % 400 == 0:
-        print("Да")
-    else:
-        print("Нет")
-print(F(2000))
