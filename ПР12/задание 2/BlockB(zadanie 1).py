7a19dbeb23890eac0cc0c482cc3ed455cd697950
ter1gan <113279557+ter1gan@users.noreply.github.com>
2022-12-03 18:05:30 +0300

    Add files via upload

---
+++ ПР12/задание 2/BlockB(zadanie 1).py
@@ -0,0 +1,28 @@
+def F():
+    x = int(input())
+    while x != 0:
+        x =int(input())
+        if x > 0:
+            max = x          
+    return max
+print(F())
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+        
+    
+         
+
+        
+        
